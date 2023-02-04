50ccc8ab232352e0f43bac5419bef475b4e0c92a
Yurii-Ilkevych <rufiktcl@gmail.com>
2023-02-04 16:46:25 +0200

    добавляє скріпт

---
+++ script.js
@@ -0,0 +1,16 @@
+function formatMessage(message, maxLength) {
+    let result;
+    // Change code below this line
+    if (message.slice < 0, maxLength)
+    {
+      result = message;
+    }
+    else if (message.slice > 0, maxLength) {
+      message =- maxLength  ;
+    }
+  
+    
+    /// Change code above this line
+    return result;
+  }
+  
